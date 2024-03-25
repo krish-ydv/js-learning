b90ca27988c057dc8a58145bbe8c3107cc01814e
krish-ydv <161123898+krish-ydv@users.noreply.github.com>
2024-03-25 13:06:52 +0545

    Update test.js

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,10 @@
 console.log("krishna")
+const accountId = 12345;
+let accountEmail = "mynbi@example.com";
+var accountPassword = "krishna123";
+accountCity = "kathmandu";
+
+//accountId=3
+accountEmail = "jhj";
+
+console.table([accountEmail, accountId, acountPassword, accountCity])
